1133bf1ccc6495120e11f0ae625fa500e28b933a
Roger Grant <rgrant@rgfm.com>
2026-01-31 15:53:55 -0800

    Update ServiceProvider.php

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -15,13 +15,6 @@ class ServiceProvider extends AddonServiceProvider
         'cp'  => __DIR__.'/../routes/cp.php',
     ];
 
-    protected $vite = [
-        'input' => [
-            'resources/js/cp.js',
-        ],
-        'publicDirectory' => 'dist',
-    ];
-
     protected $tags = [
         \Laborb\FaviconGenerator\Tags\Favicon::class,
     ];
