e40f8a191772b54eed9fe0751dd9feadee7c6521
Roger Grant <rgrant@rgfm.com>
2026-01-31 16:32:09 -0800

    v4

--- resources/js/Components/FaviconGenerator.vue
+++ resources/js/Components/FaviconGenerator.vue
@@ -1,41 +1,27 @@
 <template>
-    <div>
-        <publish-container
-            ref="container"
-            name="base"
-            :blueprint="blueprint"
-            :meta="meta"
-            v-model="values"
-        >
-            <div>
-                <div class="flex items-center justify-between mb-3">
-                    <div class="float-left">
-                        <h1>Favicon Generator</h1>
-                        <small v-if="values && values.generated_at" class="block text-xs text-gray-80">
-                            Last generated: {{ values.generated_at }}
-                        </small>
-                    </div>
-                    <button class="btn-primary" @click="save">{{ generate }}</button>
+    <publish-container
+        ref="container"
+        name="base"
+        :blueprint="blueprint"
+        :meta="meta"
+        v-model="values"
+    >
+        <div>
+            <div class="flex items-center justify-between mb-3">
+                <div class="float-left">
+                    <h1>Favicon Generator</h1>
+                    <small v-if="values && values.generated_at" class="block text-xs text-gray-80">
+                        Last generated: {{ values.generated_at }}
+                    </small>
                 </div>
-                <publish-tabs
-                    @updated="updateValue"
-                    @meta-updated="updateMeta" 
-                />
+                <button class="btn-primary" @click="save">{{ generate }}</button>
             </div>
-        </publish-container>
-        <stack v-if="isOpen" narrow name="generating-modal" @closed="isOpen = false">
-            <div class="p-6 text-center">
-                <h2 class="text-xl font-medium mb-4">Generating Favicons</h2>
-                <div role="status">
-                    <svg aria-hidden="true" class="inline w-8 h-8 animate-spin" viewBox="0 0 100 101" style="color: #eef2f6" fill="#2D9EFC" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                    </svg>
-                    <span class="sr-only">Loading...</span>
-                </div>
-            </div>
-        </stack>
-    </div>
+            <publish-tabs
+                @updated="updateValue"
+                @meta-updated="updateMeta" 
+            />
+        </div>
+    </publish-container>
 </template>
 
 <script>
@@ -49,8 +35,7 @@ export default {
     
     data() {
         return {
-            values: this.initialValues || {},
-            isOpen: false
+            values: this.initialValues || {}
         }
     },
     
@@ -70,16 +55,15 @@ export default {
         },
         
         save() {
-            this.isOpen = true;
+            this.$progress.start();
             
             Statamic.$axios.post('/cp/favicon-generator/update', this.values)
                 .then((response) => {
-                    this.isOpen = false;
+                    this.$progress.complete();
                     
                     if (response.data.status === 'success') {
                         this.$toast.success(response.data.msg || 'Favicons generated successfully');
                         
-                        // Update generated_at if returned
                         if (response.data.generated_at) {
                             this.values.generated_at = response.data.generated_at;
                         }
@@ -88,7 +72,7 @@ export default {
                     }
                 })
                 .catch((error) => {
-                    this.isOpen = false;
+                    this.$progress.complete();
                     console.error('Favicon generation error:', error);
                     this.$toast.error('Failed to generate favicons');
                 });
