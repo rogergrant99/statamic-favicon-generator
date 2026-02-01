f9744a0741c6b8005bcafb5d24814aa3ef663c92
Roger Grant <rgrant@rgfm.com>
2026-02-01 09:55:51 -0800

    a

--- resources/js/Components/FaviconGenerator.vue
+++ resources/js/Components/FaviconGenerator.vue
@@ -24,7 +24,6 @@
                 <label class="font-semibold block mb-2">API Key</label>
                 <input
                     v-model="formData.api_key"
-                    ref="apiKeyInput"
                     type="text"
                     class="input-text"
                     placeholder="Enter your RealFaviconGenerator API key"
@@ -39,7 +38,6 @@
                 <label class="font-semibold block mb-2">Master Icon URL</label>
                 <input
                     v-model="formData.icon"
-                    ref="iconInput"
                     type="url"
                     class="input-text"
                     placeholder="https://yoursite.com/assets/favicons/icon.png"
@@ -101,29 +99,9 @@ export default {
             });
         }
         console.log('Mounted with data (after Object.assign):', this.formData);
-
-        this.$nextTick(() => {
-            if (this.$refs.apiKeyInput && this.formData.api_key) {
-                this.$refs.apiKeyInput.value = this.formData.api_key;
-                console.log('Manually set API Key Input DOM value:', this.$refs.apiKeyInput.value);
-            }
-            if (this.$refs.iconInput && this.formData.icon) {
-                this.$refs.iconInput.value = this.formData.icon;
-                console.log('Manually set Icon Input DOM value:', this.$refs.iconInput.value);
-            }
-        });
-        
-        console.log('Mounted with data:', this.formData);
     },
     
     methods: {
-        getIconValue(icon) {
-            // Handle array (from assets field)
-            if (Array.isArray(icon)) {
-                return icon.length > 0 ? icon[0] : '';
-            }
-            return icon || '';
-        },
         
         save() {
             if (!this.formData.api_key?.trim()) {
