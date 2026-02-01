dec836df48ae380b10734be6a6c93931614e1b2f
Roger Grant <rgrant@rgfm.com>
2026-01-31 16:19:09 -0800

    Update index.blade.php

--- resources/views/cp/settings/index.blade.php
+++ resources/views/cp/settings/index.blade.php
@@ -1,35 +1,29 @@
 @extends('statamic::layout')
 @section('title', __('statamic-favicon-generator::cp.general.headline'))
-
 @push('head')
-    @vite(['statamic-favicon-generator/resources/js/cp.js'])
+    <script src="{{ asset('vendor/statamic-favicon-generator/js/cp.js') }}" defer></script>
 @endpush
-
 @section('content')
-    <favicon-generator
-        title="@lang('statamic-favicon-generator::cp.general.headline')"
-        :blueprint='@json($blueprint)'
-        :meta='@json($meta)'
-        :initial-values='@json($values)'
-        generate="@lang('statamic-favicon-generator::cp.general.generate')"
-    ></favicon-generator>
-
-
+<favicon-generator
+    title="@lang('statamic-favicon-generator::cp.general.headline')"
+    :blueprint='@json($blueprint)'
+    :meta='@json($meta)'
+    :initial-values='@json($values)'
+    generate="@lang('statamic-favicon-generator::cp.general.generate')"
+></favicon-generator>
 @endsection
-
 <style>
-    .animate-spin {
-        animation-name: spin;
-        animation-duration: 1s;
-        animation-iteration-count: infinite;
+.animate-spin {
+    animation-name: spin;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+}
+@keyframes spin {
+    from {
+        transform: rotate(0deg);
     }
-
-    @keyframes spin {
-        from {
-            transform: rotate(0deg);
-        }
-        to {
-            transform: rotate(360deg);
-        }
+    to {
+        transform: rotate(360deg);
     }
+}
 </style>
